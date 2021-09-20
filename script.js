4f4b66208db856acec3e9ab68103fa458acea752
Adam Gray <68389728+asg-images@users.noreply.github.com>
2021-09-20 20:34:21 +0100

    Details Update

---
+++ script.js
@@ -0,0 +1,6 @@
+const container = document.querySelector(".testimonal-content");
+const text = Array.from(container.children);
+
+
+//Width of testimonal
+const review = text[0].getBoundingClientRect().width;
