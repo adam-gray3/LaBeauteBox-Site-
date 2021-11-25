41a0ef234b93ffe084c36fbc5206f2b39dba96fb
Adam Gray <68389728+asg-images@users.noreply.github.com>
2021-11-25 22:24:58 +0000

    Image Opt/Treatments
    Optimized images, update price page and deleted unwanted files.

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
 //SETUP ARRAY OF OBJECTS FOR TESTIMONALS TO ADD DYNAMICALLY.
-const items = [{
+let items = [{
   id: 1,
   name: "Client 1",
   text: "I suffer really badly with dry and cracked skin on my feet and Emma makes them feel and look beautiful! My nails always last and look so glossy! I cant recommend her enough, she makes me feel comfortable and relaxed.",
+  image: "../images/white-towel.png"
 },
 {
   id: 2,
   name: "One of our lovely clients!",
-  text: "I've always loved Emma doing my nails as she is really gentle compared to other places i've been. She also uses really good products that last a long time and has an abundnace of colours to choose. Emma is really easy to get along with, I wouldnt use anybody else."
+  text: "I've always loved Emma doing my nails as she is really gentle compared to other places i've been. She also uses really good products that last a long time and has an abundnace of colours to choose. Emma is really easy to get along with, I wouldnt use anybody else.",
+  image: "../images/white-hand.png"
 },
 {
   id: 3,
   name: "Client 3",
-  text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ea nobis placeat, vitae ullam necessitatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ea nobis placeat, vitae ullam necessitatibus"
+  text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ea nobis placeat, vitae ullam necessitatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ea nobis placeat, vitae ullam necessitatibus",
+  image: "../images/white-spa.png"
 }
 ];
+
+
 
 //SETUP VARIABLES FOR ALL DOM ITEMS AND START POINT FOR slideshow
 let currentSlide = 0;
 
 const name = document.querySelector(".name");
 const testimonal = document.querySelector(".testimonal");
+const icon = document.querySelector(".icon");
 
 const nxtBtn = document.querySelector(".next-btn");
 const prevBtn = document.querySelector(".prev-btn");
@@ -30,6 +36,7 @@ function showSlide(){
   /*const slides = items[currentSlide];*/
   name.textContent = items[currentSlide].name;
   testimonal.textContent = items[currentSlide].text;
+  icon.src = items[currentSlide].image;
 }
 
 //SETUP EVENT LISTENERS FOR BUTTONS
