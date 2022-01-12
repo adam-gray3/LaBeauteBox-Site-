cca43eae970e94be89053576c6592ed89df28481
Adam Gray <68389728+asg-images@users.noreply.github.com>
2022-01-12 20:24:39 +0000

    Picture angle test

--- form.js
+++ form.js
@@ -1,28 +1,11 @@
-
 const form = document.querySelector(".form");
 const name = document.querySelector(".name");
 const email = document.querySelector(".email");
 const message = document.querySelector(".message");
 
-
 form.addEventListener("submit", checkAll);
 
-const nameError = "Enter valid name!";
-
-form.addEventListener("kwCustomRuleSet", function(){
-  window.setCustomKwRule(
-    "form",
-    "name",
-    nameError,
-    function(){
-      const nameValue = name.value.trim();
-      if(nameValue === ""){
-        showError();
-      }
-    }
-  )
-});
-
+let pass = true;
 //functions
 //ERROR FUNCTION
 function showError(input, message){
@@ -36,31 +19,39 @@ function showError(input, message){
 function checkName(input){
   if(input === ""){
     showError(name, "Enter a valid name!");
-
-  }
+    return false;
+  } else{return}
 };
 
 function checkEmail(input){
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if(!re.test(input)){
       showError(email, "Enter a valid email!");
+      return false;
+    } else{
+    return;
     }
 };
 
 function checkMessage(input){
   if(input === ""){
     showError(message, "Enter a small message!");
+    return false;
+  } else{
+    valid()
   }
 }
-
 
 function checkAll(e){
   e.preventDefault();
   const nameValue = name.value.trim();
   const emailValue = email.value.trim();
   const messageValue = message.value;
-
   checkName(nameValue)
   checkEmail(emailValue)
   checkMessage(messageValue)
+}
+
+function valid(){
+  console.log("valid")
 }
